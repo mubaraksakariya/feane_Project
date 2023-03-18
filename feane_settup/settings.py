3bb6666f4a521cbc17629f1bab1b8890418b0628
mubaraksakariya <mubaraksakariya@gmail.com>
2023-03-19 01:54:27 +0530

    Fresh Update
    Deleted all migration files

--- feane_settup/settings.py
+++ feane_settup/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-3bev!&8d$uc2fdg7arb#1igk&%-c4xxip9klmi!(e0sx+7dz8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '13.233.224.3']
 
 
 # Application definition
